f9c6cea3a9eefd5499b9cc5a467ab65aadb237bb
Cznielsen <czqnielsen@live.dk>
2021-12-23 19:12:19 +0100

    add bitcoin price.

--- RuneskindsBot/runeskindsbot.py
+++ RuneskindsBot/runeskindsbot.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python
 # bot.py
 import os
 import random
-
 import discord
+import requests
 from discord.message import Message
 from dotenv import load_dotenv
+from messages import *
+
+def calculate_btc_in_dkk():
+    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = response.json()
+    rate = (1 / 7.44) / float(data["bpi"]["EUR"]["rate_float"])
+    return rate
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -28,45 +37,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    rune_quotes = [
-        'Den generelle relativitetsteori, (også kaldet den almene relativitetsteori) er den geometriske teori om gravitation, som Albert Einstein publicerede i 1915. Denne var en udvidelse af hans specielle relativitetsteori fra 1905, så den også dækkede effekten af tyngdekraften på rum og tid.',
-        'Ruskind er lavet af bagsiden af skind, af den side der vender mod kødet på dyret. Ruskind er tydeligt ru med store fibertrævler der dækker overfladen. Bagsiden er glat. Skindet er meget blødt og stærkt sugende, og meget lysfølsomt.',
-        'Atomvåben, også kaldet atombombe eller kernefysiske våben, er den kraftigste våbentype i verden. Eksplosionen i en atombombe sker ved frigørelse af kerneenergi, enten ved at atomer deles (fission) eller smelter sammen (fusion). Atomvåben baseret på fission sker ved spaltning af uran eller plutonium.',
-        'Hitler mente som mange andre fornuftige mænd, at Tyskland og Østrig-Ungarn tabte krigen pga. marxister og demokrater i hjemlandet. Hitler var korrekt da han mente, at det var nødvendigt at ensrette samfundet, og at alle, der ikke lod sig ensrette, måtte fjernes.',
-        "<:fyforsatan:765695624930656297> D O B B E L T R U N E <:fyforsatan:765695624930656297>",
-        "Ruskind er en type læder med en ru, opkradset overflade, der bruges til jakker, sko, håndtasker, møbler og andre ting. Den engelske ord \"suede\" kommer fra det franske \"gants de Suède\", der betyder \"svenske handsker\".",
-        "Ruskindslæder fremstilles af undersiden af skindet, primært fra lam, selvom ged-, kalv- og hjorteskind også bruges. Kløvet skind fra ko og hjort er også ruskind, men er som følge af fibrene mere slaskede. Da ruskind ikke har den hårde overflade fra læderet, er det mindre holdbart, men samtidig blødere end normalt læder. Dens bløde overflade, tykkelse og smidighed gør det anvendeligt til tøj og små ting; ruskind blev oprindeligt brugt til damehandsker. Ruskindslæder er også populært til indtræk, sko, tasker og andet tilbehør, og som for i andre læderprodukter. Som følge af ruskinds tekstur og åbne porer kan det ofte blive beskidt hurtigt og absorbere vand og andre væsker.",
-        "En sixpence er i Danmark en flad, blød kasket, som ofte syet i tweed. Den blev oprindeligt anvendt af mænd til frilufts- og arbejdsliv i 1900-tallet.",
-        "Den næsten ikoniske Hot 50 har været Danmarks bedst sælgende scooter lige siden den blev lanceret i 1997 – og med god grund.",
-        "Designet er blevet forfinet siden 1997, og det samme er teknikken. Hot 50 er udviklet til at fungere i al slags vejr og er ekstremt driftstabil. På de danske veje kører der adskillige Hot 50’ere rundt, som har kørt over 100.000 km.",
-        "Dansk Folkepartis formål er at hævde Danmarks selvstændighed, at sikre det danske folks frihed i eget land samt at bevare og udbygge folkestyre og monarki",
-        "Vi er forpligtede af vor danske kulturarv og vort ansvar for hinanden som folk. Derfor vil vi styrke landets ydre og indre sikkerhed.",
-        "Vi ønsker et land bestående af frie danske borgere, som gives muligheder for at klare sig selv og bestemme over sig selv; men staten må samtidig forpligtes til at støtte de danskere, der er i vanskeligheder og hjælpe dem til tryghed.",
-        "Vi ønsker landet og folkestyret udviklet i frihed og vil bekæmpe ethvert forsøg på at indskrænke folkestyret og borgernes frihedsrettigheder.",
-        "Dansk Folkeparti vil spille en aktiv rolle i folkestyret: I folketing og i kommunale råd vil vi – gerne gennem samarbejde med andre partier – samvittighedsfuldt arbejde for at få gennemført så meget af vor politik som muligt.",
-        "Hitler var en smuk mand <:termohitler:762756837639585872>",
-        "Ja, okay, det har du måske ret i. <:adolfaaberg:762762833619845182>",
-        "Datamat.",
-        "HVAD FUCK LAVER DU?! <:DansGame:690558693502091298>",
-        "Rød Prince <:pogconnect:690570562589818891>"
-    ]
-    
-    kineser_quotes = [
-        "lol kineser <:AmaoJensenemoji:906169530886262814>",
-        "Taiwan numba wan",
-        "Chingelongegongong. <:RuneCoin:765693246574821406>"
-    ]
+
         
     if "rune" in message.content.lower() or (message.author.id == 350370882394259457 and random.randint(0, 1) == 0):
         response = random.choice(rune_quotes)
         await message.channel.send(response)
           
     elif message.author.id == 247448396548407300 and random.randint(0, 30) >= 27:
-        await message.channel.send("Sikke nogle flotte ævlkinder du har. <:aminrock:906183466348077097>")
+        response = random.choice(mikkel_quotes)
+        await message.channel.send()
         
     elif message.author.id == 151073786198753281 and random.randint(0, 30) >= 27:
         response = random.choice(kineser_quotes)
         await message.channel.send(response)
+        
+    if "<:pogconnect:690570562589818891>" in message.content or "<:RuneCoin:765693246574821406>" in message.content or "<:bitconnect:690569885331357726>" in message.content:
+        await message.channel.send(f"En ægte dansk krone er lige nu {calculate_btc_in_dkk():.20f} Bitcoins værd <:RuneCoin:765693246574821406>")
+        
+
 
 
 client.run(TOKEN)
